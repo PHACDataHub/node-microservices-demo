c40fc450324f6d40aaf93002a5c8c2e61975b52b
Mike Williamson <mike@korora.ca>
2022-10-13 18:20:48 -0400

    Switch to GraphQL Yoga
    This commit drops the zombie express-graphql middleware in favour of
    GraphQL Yoga.

--- api/src/Server.js
+++ api/src/Server.js
@@ -1,41 +1,20 @@
 import express from 'express'
-import { graphqlHTTP } from 'express-graphql'
-import { createComplexityRule } from 'graphql-query-complexity'
-
-// https://github.com/slicknode/graphql-query-complexity/issues/65
-function simpleEstimator({ defaultComplexity = 1 }) {
-  return (args) => {
-    return defaultComplexity + args.childComplexity
-  }
-}
+import { createServer } from '@graphql-yoga/node'
 
 export function Server({
   schema,
-  root: rootValue,
-  maximumComplexity = 25,
-  context = {},
+  context = {
+    log: (...args) => console.log(args),
+  },
 }) {
-  const server = express()
+  const app = express()
 
-  server.use(
-    '/',
-    graphqlHTTP(async (_req, _res, { variables }) => ({
-      schema,
-      rootValue,
-      context,
-      graphiql: true,
-      validationRules: [
-        createComplexityRule({
-          estimators: [
-            // Configure your estimators
-            simpleEstimator({ defaultComplexity: 1 }),
-          ],
-          maximumComplexity,
-          variables,
-        }),
-      ],
-    })),
-  )
+  const graphQLServer = createServer({
+    schema,
+    context,
+  })
 
-  return server
+  app.use('/', graphQLServer)
+
+  return app
 }
--- api/src/__tests__/Server.test.js
+++ api/src/__tests__/Server.test.js
@@ -1,29 +1,29 @@
+import { makeExecutableSchema } from '@graphql-tools/schema'
 import request from 'supertest'
 import { Server } from '../Server.js'
 // https://github.com/facebook/jest/issues/9430#issuecomment-616232029
 // eslint-disable-next-line
 import { jest } from '@jest/globals' // support for ESM modules
 
-import { buildSchema } from 'graphql'
-
 // Construct a schema, using GraphQL schema language
-const schema = buildSchema(`
+const typeDefs = `
   type Query {
     hello: String
   }
-`)
+`
 
-// The root provides a resolver function for each API endpoint
-const root = {
-  hello: () => {
-    return 'Hello world!'
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
   },
 }
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 describe('Server', () => {
   describe('given a schema and root', () => {
     it('returns an express server', async () => {
-      const server = new Server({ schema, root })
+      const server = await Server({ schema })
 
       const response = await request(server)
         .post('/')
@@ -33,39 +33,6 @@ describe('Server', () => {
         })
 
       expect(response.body).toEqual({ data: { hello: 'Hello world!' } })
-    })
-  })
-
-  describe('given an overly complex query', () => {
-    it('rejects it', async () => {
-      const server = new Server({ schema, root, maximumComplexity: 1 })
-
-      const response = await request(server)
-        .post('/')
-        .set('Accept', 'application/json')
-        .send({
-          query: '{hello hello}',
-        })
-
-      const [err] = response.body.errors
-      expect(err.message).toEqual(
-        'The query exceeds the maximum complexity of 1. Actual complexity is 2',
-      )
-    })
-  })
-
-  describe('given a simple query', () => {
-    it('executes it', async () => {
-      const server = new Server({ schema, root, maximumComplexity: 1 })
-
-      const response = await request(server)
-        .post('/')
-        .set('Accept', 'application/json')
-        .send({
-          query: '{hello}',
-        })
-
-      expect(response.body).not.toHaveProperty('errors')
     })
   })
 })
